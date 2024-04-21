6dccb9a17bffea5132c49874d416a5874be7edfb
Koncz Máté <h265733@stud.u-szeged.hu>
2024-04-21 14:59:38 +0200

    hello vilag

--- home/views.py
+++ home/views.py
@@ -1,4 +1,9 @@
+from django.template import loader
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+def hello(request):
+    template = loader.get_template("homeTemplate.html")
+    return HttpResponse(template.render())
